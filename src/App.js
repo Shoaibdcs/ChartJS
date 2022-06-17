f86df4b38b44fcb8079bdc088cdf9718f41b9eb9
Talal_Khan <k181251@nu.edu.pk>
2022-06-17 14:05:52 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Hello World!!
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './component/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
